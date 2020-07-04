91963e30f07937e8b3e41ea18d7e7791dc6c2517
Procok <szeplakig91@gmail.com>
2020-07-04 10:35:45 +0200

    navigation2

--- pages/components/toolbar.vue.js
+++ pages/components/toolbar.vue.js
@@ -1,30 +1,40 @@
 Vue.component("spa-toolbar", {
   template: `
         <div>
-            <v-toolbar dark color="primary" class="mb-2">
-                <v-layout align-center>
-                    <v-spacer></v-spacer>
-                    <router-link to="/"><v-toolbar-title class="white--text">StoryBoard</v-toolbar-title></router-link>
-                </v-layout>
-            </v-toolbar>
+            <v-app-bar
+                app
+                dark
+                nav
+            >
+                <v-spacer></v-spacer>
+                <v-toolbar-title nav class="white--text"><router-link to="/">StoryBoard</router-link></v-toolbar-title>
+            </v-app-bar>
+        
             <v-navigation-drawer
                 permanent
                 expand-on-hover
                 app
-                >
+            >
                 <v-list
                     nav
                     dense
                 >
+                    <v-list-item link to="/">
+                        <v-list-item-icon>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+                
                     <v-list-item link to="/story">
                         <v-list-item-icon>
-                            <v-icon>mdi-folder</v-icon>
+                            <v-icon>mdi-folder-text</v-icon>
                         </v-list-item-icon>
-                        <v-list-item-title>Story</v-list-item-title>
+                        <v-list-item-title>Board</v-list-item-title>
                     </v-list-item>
                     <v-list-item link to="/settings">
                         <v-list-item-icon>
-                            <v-icon>mdi-star</v-icon>
+                            <v-icon>mdi-cog</v-icon>
                         </v-list-item-icon>
                         <v-list-item-title>Settings</v-list-item-title>
                     </v-list-item>
@@ -32,4 +42,5 @@ Vue.component("spa-toolbar", {
             </v-navigation-drawer>
         </div>
 `,
+  data: () => ({}),
 });
